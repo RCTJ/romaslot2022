019e2e5cac4f4b02f007ce530d3a7ccc503be852
SBOAAAA <fun_pu@hotmail.com>
2022-11-23 16:03:34 +0700

    edit

---
+++ spec/romaslot2022-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Romaslot2022View from '../lib/romaslot2022-view';
+
+describe('Romaslot2022View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
